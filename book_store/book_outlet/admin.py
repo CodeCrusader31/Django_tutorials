4a329c8cc518a00dfcd6c5271783ad36d8232015
CodeCrusader31 <niteshwarkr2056@iiitmanipur.ac.in>
2025-12-20 14:14:55 +0530

    Admin

---
+++ book_store/book_outlet/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from.models import Book
+
+# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    readonly_fields = ('slug',)
+
+admin.site.register(Book)
